ce239e71d8259374dcac65b35dc40ca3f11717bc
xinghalo <xinghalo@163.com>
2017-07-21 23:31:49 +0800

    样例提交

---
+++ src/main/java/test/Application.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by xinghailong on 2017/7/21.
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/test/web/TestController.java
@@ -0,0 +1,15 @@
+package test.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by xinghailong on 2017/7/21.
+ */
+@RestController
+public class TestController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
+}
